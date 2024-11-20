8ebe30f562449ed00a82d2e7e06fb7cbf2c03872
Sanjana Mathi <sanjuvimal@icloud.com>
2024-11-20 18:55:46 +0100

    added add_students, view_all_students methods

--- student_management.py
+++ student_management.py
@@ -10,6 +10,11 @@ class Student:
         self.grade = grade
         self.subjects = subjects
 
+    def print_student(self):  # to print the details of the student
+        print(
+            f"ID: {self.id}, Name: {self.name}, Age: {self.age}, Grade: {self.grade}, Subjects: {', '.join(self.subjects)}"
+        )
+
 
 # this manages students data (multiple students)
 class Students:
@@ -17,17 +22,18 @@ class Students:
         self.student_list = []  # here im storing instance of 'class Student'
         self.load_students_from_file()
 
-    # "students = [{"id":1,"name":"sanjana","age":30,"grade":"Vg","subject":["math","english"]},
-    # {"id":1,"name":"sanjana","age":30,"grade":"Vg","subject":["math","english"]}]"
+    def add_student(self, student_data):
+        student = Student(student_data)
+        self.students.append(student)
+        print(f"Student {student.name} added successfully.")
 
-    def add_student(self):
-        student_data = self.get_input()
-        if student_data:
-            self.student_list.append(student_data)
-
-    def view_students():
-        for student in Student.student_lsit:
-            pass
+    def view_all_students(self):
+        if not self.students:
+            print("No students available.")
+        else:
+            for student in self.students:
+                student_obj = Student(student)
+                student_obj.print_student()
 
     def update_student():
         pass
@@ -42,29 +48,30 @@ class Students:
         pass
 
 
-student = Students()
-
-
-def get_input():
+# getting user input and validating - done but needs more conditons for validation
+def get_input(students):
     try:
-        subject = []
-        id = int(input("Enter student id: "))  # check condition for unique id
+        subjects = []
+        student_id = int(input("Enter student id: "))
+        for student in students:
+            if any(student["id"] == student_id):  # check condition for unique id
+                print(f"Student ID {student_id} already exists.")
         name = input("Enter student name: ")
         age = int(input("Enter student age: "))
         grade = input("Enter student grade: ")
         num_of_subjects = int(input("Enter how many subjects: "))
         for n in range(0, num_of_subjects):
             n = input("Enter subject: ")
-            subject.append(n)
-
-        student.add_student(id, name, age, grade, subject)
+            subjects.append(n)
+        return {"id": student_id, "name": name, "age": age, "grade": grade, "subjects": subjects}
 
     except ValueError:
         print("Enter valid data.")
 
 
+# user options to perform desired operation. - done
 def options():
-    students = Students()
+    students_manager = Students()
     while True:
         print("""Choose a function from below options list:
                             1. Add student
@@ -76,19 +83,27 @@ def options():
             option = int(input(">"))
             if option == 1:
                 print("You chose to Add student.\nEnter student details.")
-                student_data = get_input(students.student_list)
-                if student_data:
-                    students.add_student(student_data)
+                student_data = get_input(students_manager.students)  # Get input and validate ID
+                students_manager.add_student(student_data)
 
             elif option == 2:
-                student.view_students()
+                students_manager.view_all_students()
+
             elif option == 3:
-                student.update_student()
+                student_id = int(input())
+                students_manager.update_student(student_id)
+
             elif option == 4:
-                student.delete_student()
+                try:
+                    student_id = int(input("Enter ID of the student to delete: "))
+                    students_manager.delete_student(student_id)
+                except ValueError:
+                    print("Invalid input. ID must be number.")
+
             elif option == 5:
-                student.save_students_to_file()
+                students_manager.save_students_to_file()
                 break
+
             else:
                 print("Invalid option.")
 
