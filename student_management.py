42837545e3d0f90cd7fae0d599d84164014f629c
DurgaDivya <divya.pallantla@gmail.com>
2024-12-04 15:28:51 +0100

    latest changes in the code

--- student_management.py
+++ student_management.py
@@ -42,16 +42,40 @@ class Students:
                 student.print_student()
 
     def update_name(self, id, name):
-        pass
+        for student in self.student_list:
+            if student.id == id:
+                student.name = name
+                print(f"Student ID {id}'s name updated to {name}.")
+                self.save_students_to_file()
+                return
+        print(f"Student with ID {id} not found.")
 
     def update_age(self, id, age):
-        pass
+        for student in self.student_list:
+            if student.id == id:
+                student.age = age
+                print(f"Student ID {id}'s age updated to {age}.")
+                self.save_students_to_file()
+                return
+        print(f"Student with ID {id} not found.")
 
     def update_grade(self, id, grade):
-        pass
+        for student in self.student_list:
+            if student.id == id:
+                student.grade = grade
+                print(f"Student ID {id}'s grade updated to {grade}.")
+                self.save_students_to_file()
+                return
+        print(f"Student with ID {id} not found.")
 
     def update_subjects(self, id, subjects):
-        pass
+        for student in self.student_list:
+            if student.id == id:
+                student.subjects = subjects
+                print(f"Student ID {id}'s subjects updated to {', '.join(subjects)}.")
+                self.save_students_to_file()
+                return
+        print(f"Student with ID {id} not found.")
 
     def delete_student(self, student_id: int):
         for student in self.student_list:
@@ -92,7 +116,7 @@ class Students:
         print("ID", "Name", "Age", "Grade", "Subjects")
         while True:
             try:
-                feild = input("Enter field to update: ")
+                feild = input("Enter field to update: (type 'exit' to stop)")
                 if not feild:
                     print("Feild cannot be empty.")
                     continue
@@ -109,11 +133,18 @@ class Students:
                     elif feild == "grade":
                         value = self.get_grade()
                         self.update_grade(id, value)
-                    else:
+                    elif feild == "subjects":
                         value = self.get_subjects()
                         self.update_subjects(id, value)
-            except ValueError:
-                print("Enter valid field.")
+                    elif feild == "exit":
+                        return
+            
+                    else:
+                        print("Invalid field. Please choose: Name, Age, Grade, Subjects")
+                
+            except ValueError as e:
+                print(f"Error: {e}. Please try again.")
+
 
     def get_student_data(self):
         id = self.get_id()
@@ -190,7 +221,7 @@ class Students:
             else:
                 new_list.append(sub)
                 n += 1
-        return new_list
+        return new_list 
 
 
 FILE_NAME = "student_data.json"
